101018bff59a07ed5799609d5e2019ad251d22ce
Asiama Phoebe <asiama.phoebe@hubtel.com>
2024-07-18 20:23:40 +0000

    Quiz Error and Calculator

--- quiz.js
+++ quiz.js
@@ -1,17 +1,14 @@
-function checkAnswer(){
+function checkAnswer() {
+  var correctAnswer = "4";
 
-      var correctAnswer = "4";
+  var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-      var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
-
-      if(userAnswer === correctAnswer){
- 
-            document.getElementById('feedback').textContent = "Correct! Well done";
-
-      }else{
-            document.getElementById('feedback').textContent = "That's incorrect. Try again!";
-      }
+  if ( userAnswer === correctAnswer ) {
+    document.getElementById("feedback").textContent = "Correct! Well done";
+  } else {
+    document.getElementById("feedback").textContent =
+      "That's incorrect. Try again!";
+  }
 }
 
-
-document.getElementById('submit-answer').addEventListener('click', checkAnswer);
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
